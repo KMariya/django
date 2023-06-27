ad9236f8d0f17dbde7290b0c9551da0b74181208
Mariya Kozharinova <0rini0mk@gmail.com>
2023-06-27 18:23:31 +0300

    правки

--- coursework_django/myapp/admin.py
+++ coursework_django/myapp/admin.py
@@ -3,34 +3,28 @@ from django.contrib import admin
 from .models import News
 from .models import Rest
 from .models import Comment
-from django.utils.html import format_html
 from .models import ContactMessage
 
-
-
-
 class NewsAdmin(admin.ModelAdmin):
-    list_display = ('author', 'image', 'title', 'text', 'created_date', 'published_date')
+    list_display = ('id', 'author', 'image', 'title', 'text', 'created_date', 'published_date')
     list_filter = ['author', 'created_date']
     date_hierarchy = 'created_date'
-
 
 admin.site.register(News, NewsAdmin)
 
 class RestAdmin(admin.ModelAdmin):
-    list_display = ('author', 'title', 'text')
+    list_display = ('id', 'author', 'title', 'text')
 
 admin.site.register(Rest, RestAdmin)
 
 class CommentAdmin(admin.ModelAdmin):
-    list_display = ('author', 'post', 'content', 'created_at')
+    list_display = ('id', 'author', 'post', 'content', 'created_at')
     list_filter = ['author', 'created_at', 'post']
     date_hierarchy = 'created_at'
 
 admin.site.register(Comment, CommentAdmin)
 
 
-
 class ContactMessageAdmin(admin.ModelAdmin):
-    list_display = ('name', 'phone', 'message')
+    list_display = ('id', 'name', 'phone', 'message')
 admin.site.register(ContactMessage, ContactMessageAdmin)
--- coursework_django/polls/admin.py
+++ coursework_django/polls/admin.py
@@ -1,17 +1,22 @@
 from django.contrib import admin
 from .models import Question, Choice
-
-
-class QuestionAdmin(admin.ModelAdmin):
-    list_display = ('question_text', 'pub_date')
-    list_filter = ['pub_date']
-    date_hierarchy = 'pub_date'
-
-admin.site.register(Question, QuestionAdmin)
-
-
 class ChoiceAdmin(admin.ModelAdmin):
-    list_display = ('question', 'choice_text', 'votes')
+    list_display = ('id', 'question', 'choice_text', 'votes')
     list_filter = ['question', 'choice_text', 'votes']
 
 admin.site.register(Choice, ChoiceAdmin)
+
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 2
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'question_text', 'pub_date')
+    list_filter = ['pub_date']
+    date_hierarchy = 'pub_date'
+    inlines = [ChoiceInline]
+    search_fields = ['question_text']
+
+admin.site.register(Question, QuestionAdmin)
